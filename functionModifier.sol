c05d436fa43ec0087bacdd0920e754bebf28d8ad
akshajchainani <akshajchainani730@gmail.com>
2025-07-23 14:30:27 +0530

    main

---
+++ functionModifier.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Visibility {
+    address owner;
+    uint result;
+
+    // Constructor to set the owner
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // Modifier to check if the caller is the owner
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    // Function to set the result, restricted to the owner
+    function setResult(uint _result) public onlyOwner {
+        result = _result;
+    }
+}
